5e83c6e8577df1e7430efbe67d2b621b0997b663
dspdamith <dspdamithse@gmail.com>
2019-05-16 16:10:38 +0530

    Create First View And Pass Data from Controller And Use Those Data In View

---
+++ EMovie/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EMovie.Models;
+
+namespace EMovie.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie {Name ="Damith Movie" };
+            return View(movie);
+        }
+    }
+}
