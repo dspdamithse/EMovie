0dd8090c433b9d114e6afdbdc21d3a6486e42b61
dspdamith <dspdamithse@gmail.com>
2019-05-16 17:55:13 +0530

    Random Movie View With ViewModel

--- EMovie/Controllers/MoviesController.cs
+++ EMovie/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using EMovie.Models;
+using EMovie.ViewModels;
 
 namespace EMovie.Controllers
 {
@@ -13,7 +14,18 @@ namespace EMovie.Controllers
         public ActionResult Random()
         {
             var movie = new Movie {Name ="Damith Movie" };
-            return View(movie);
+
+            var customers = new List<Customer>{
+                new Customer { Name = "Damith Chandrasekara"},
+                new Customer { Name = "Chandana Prasantha"}
+            };
+            var viewModel = new RandomViewModel
+            {
+                Movies = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
     }
 }
---
+++ EMovie/ViewModels/RandomViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EMovie.Models;
+
+namespace EMovie.ViewModels
+{
+    public class RandomViewModel
+    {
+        public Movie Movies { get; set; }
+        public List<Customer> Customers { get; set; }
+
+    }
+}
